05689f5936a876a1e932f11fb250d7951a4b5238
marcllopisraja-crypto <marc.llopis.raja@gmail.com>
2026-07-15 09:41:35 +0200

    Update ping.py

--- .github/workflows/ping.py
+++ .github/workflows/ping.py
@@ -1,7 +1,7 @@
 import requests
 import sys
 
-url = "https://bigrocks.streamlit.app/" # La web que vols comprovar
+url = "https://bigrocks.streamlit.app/"
 
 try:
     response = requests.get(url, timeout=10)
@@ -9,7 +9,7 @@ try:
         print(f"La web {url} està activa.")
     else:
         print(f"La web ha respost amb el codi: {response.status_code}")
-        sys.exit(1) # Això farà que l'acció falli i t'avisin
+        sys.exit(1)
 except Exception as e:
     print(f"Error connectant a {url}: {e}")
     sys.exit(1)
